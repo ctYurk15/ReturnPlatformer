b2338d952d8f0269b5be2133c3b221036478d2ab
ctYurk15 <levgenetic@gmail.com>
2021-09-20 18:36:30 +0300

    Added moving gameobjects

---
+++ Assets/Scripts/StickyPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    [SerializeField] GameObject acceptedGameObject;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == acceptedGameObject.name)
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == acceptedGameObject.name)
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
---
+++ Assets/Scripts/WaypointMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMovement : MonoBehaviour
+{
+    [SerializeField] GameObject[] waypoints;
+    int currentWaypointIndex = 0;
+
+    [SerializeField] float speed = 1f;
+
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < 0.1f)
+        {
+            currentWaypointIndex++;
+
+            //if we are at the last waypoint
+            if(currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
+    }
+}
